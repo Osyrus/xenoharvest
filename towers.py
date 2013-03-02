14e303b31adae59ba969e77bbbbfd2ca8c55101d
Myles Clark <blokart11@gmail.com>
2013-03-02 17:06:59 +1030

    Added skeleton code for extra tower types

--- towers.py
+++ towers.py
@@ -3,9 +3,10 @@ class Tower:
   def __init__(self, x, y):
     self.pos = x, y
     self.type = 0
+    self.radius = 0
 
   def getPos(self):
-    return pos
+    return self.pos
 
   def getType(self):
     return self.type
@@ -14,3 +15,28 @@ class LaserTower(Tower):
   def __init__(self, x, y):
     super(LaserTower, self).__init__(x, y)
     self.type = 1
+
+class FlameTower(Tower):
+  def __init__(self, x, y):
+    super(FlameTower, self).__init__(x, y)
+    self.type = 2
+
+class CannonTower(Tower):
+  def __init__(self, x, y):
+    super(CannonTower, self).__init__(x, y)
+    self.type = 3
+
+class RocketTower(Tower):
+  def __init__(self, x, y):
+    super(RocketTower, self).__init__(x, y)
+    self.type = 4
+
+class SlowTower(Tower):
+  def __init__(self, x, y):
+    super(SlowTower, self).__init__(x, y)
+    self.type = 5
+
+class LureTower(Tower):
+  def __init__(self, x, y):
+    super(LureTower, self).__init__(x, y)
+    self.type = 6
