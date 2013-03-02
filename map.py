734a865dd558e87769970dd2bfd4a7ae2ac6ea40
Myles Clark <blokart11@gmail.com>
2013-03-02 14:56:09 +1030

    Added basic tile generator functions into map.py, removed unneccesary tile.py
    Also modified git ignore to stop it from adding the silly OSX meta file
    .DS_Store.

--- map.py
+++ map.py
@@ -1,8 +1,36 @@
 from tile import *
+import random
 
 class Map:
   def __init__(self, w, h):
-    self.tiles = [[Tile()]*w]*h
+    self.tiles = [[0]*w]*h
     self.width = w
     self.height = h
 
+    self.numTiles = 10
+    self.tileWeighting = [50, 50, 50, 50, 50, 1, 1, 5, 4, 1]
+    _normWeight(self.tileWeighting)
+
+    self.fill()
+
+  def fill(self):
+    for x in range(self.width):
+      for y in range(self.height):
+        self.tiles[y][x] = _chooseTile()
+
+  def _chooseTile():
+    rand = random.random()
+
+    for i in range(len(self.tileWeighting)):
+      if rand < self.tileWeighting[i]:
+        return i
+      else:
+        rand -= self.tileWeighting[i]
+
+    return 0
+
+  def _normWeight(array):
+    total = sum(array)*1.0
+
+    for x in range(0, self.numTiles):
+      array[x] = array[x]/total
--- tile.py
+++
@@ -1,8 +0,0 @@
-
-class Tile:
-
-  def __init__(self):
-    self.type = 0
-
-  def setType(self, i):
-    self.type = i
