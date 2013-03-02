f281856df02f5c837516a9c32c8f65779df65959
Myles Clark <blokart11@gmail.com>
2013-03-02 13:47:13 +1030

    Added map.py and tile.py, changed indents to two spaces.

--- main.py
+++ main.py
@@ -9,45 +9,45 @@ self_id = -1
 byteRec = 1024
 
 def parse(data):
-    global self_id
+  global self_id
 
-    cmd = data[0]
-    msg = data[1:]
+  cmd = data[0]
+  msg = data[1:]
 
-    if cmd == 'c':
-        self_id = int(msg)
-    elif cmd == 't':
-        params = msg.split(',',1)
-        id = int(params[0])
-        if id != self_id:
-            print "[Player "+str(id+1)+"] "+params[1]
+  if cmd == 'c':
+    self_id = int(msg)
+  elif cmd == 't':
+    params = msg.split(',',1)
+    id = int(params[0])
+    if id != self_id:
+      print "[Player "+str(id+1)+"] "+params[1]
 
 def serverlisten(socket):
-    
-    while True:
-        data = socket.recv(byteRec)
-        if not data:
-            break
-        cmds = data.split(';')
-        for i in cmds:
-            if i:
-                parse(i)
+  
+  while True:
+    data = socket.recv(byteRec)
+    if not data:
+      break
+    cmds = data.split(';')
+    for i in cmds:
+      if i:
+        parse(i)
 
 ip   = raw_input("Server ip: ");
 port = input("Server port: ");
 try:
-    #create an AF_INET, STREAM socket (TCP)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  #create an AF_INET, STREAM socket (TCP)
+  s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except socket.error, msg:
-    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
-    sys.exit()
+  print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+  sys.exit()
 
 print 'Socket Created'
 s.connect((ip,port))
 start_new_thread(serverlisten ,(s,))
 while 1:
-    msg = raw_input()
-    msg.replace(';','')
-    s.send("t"+msg)
+  msg = raw_input()
+  msg.replace(';','')
+  s.send("t"+msg)
 
 s.close()
