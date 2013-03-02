6db76ed8af58c5ee0cb481c7c92e7344683a68ed
Myles Clark <blokart11@gmail.com>
2013-03-02 15:54:13 +1030

    Fixed indentation again

--- client.py
+++ client.py
@@ -9,27 +9,27 @@ self_id = -1
 
 def serverlisten(socket,event):  
   while True:
-    data = socket.recv(1024)
-    if not data:
-      break
-    cmds = data.split(';')
-    for i in cmds:
-      if i:
-        event.notify("cmd_recv", i)
-				
+  data = socket.recv(1024)
+  if not data:
+    break
+  cmds = data.split(';')
+  for i in cmds:
+    if i:
+    event.notify("cmd_recv", i)
+        
 def connect(ip, port, event):
-	try:
-		#create an AF_INET, STREAM socket (TCP)
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	except socket.error, msg:
-		print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
-		sys.exit()
+  try:
+    #create an AF_INET, STREAM socket (TCP)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  except socket.error, msg:
+    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+    sys.exit()
 
-	print 'Socket Created'
-	s.connect((ip,port))
-	start_new_thread(serverlisten ,(s,event,))
-	return s
-	
+  print 'Socket Created'
+  s.connect((ip,port))
+  start_new_thread(serverlisten ,(s,event,))
+  return s
+  
 def disconnect(s):
-	s.sendall("d")
-	socket.close()
+  s.sendall("d")
+  socket.close()
