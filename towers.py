4658e0a245941253f8dad65610659fc8df2226d9
Myles Clark <blokart11@gmail.com>
2013-03-02 17:26:42 +1030

    Changed superclass call in tower.py

--- towers.py
+++ towers.py
@@ -13,30 +13,30 @@ class Tower:
 
 class LaserTower(Tower):
   def __init__(self, x, y):
-    super(LaserTower, self).__init__(x, y)
+    Tower.__init__(x, y)
     self.type = 1
 
 class FlameTower(Tower):
   def __init__(self, x, y):
-    super(FlameTower, self).__init__(x, y)
+    Tower.__init__(x, y)
     self.type = 2
 
 class CannonTower(Tower):
   def __init__(self, x, y):
-    super(CannonTower, self).__init__(x, y)
+    Tower.__init__(x, y)
     self.type = 3
 
 class RocketTower(Tower):
   def __init__(self, x, y):
-    super(RocketTower, self).__init__(x, y)
+    Tower.__init__(x, y)
     self.type = 4
 
 class SlowTower(Tower):
   def __init__(self, x, y):
-    super(SlowTower, self).__init__(x, y)
+    Tower.__init__(x, y)
     self.type = 5
 
 class LureTower(Tower):
   def __init__(self, x, y):
-    super(LureTower, self).__init__(x, y)
+    Tower.__init__(x, y)
     self.type = 6
