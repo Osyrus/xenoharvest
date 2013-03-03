c67040cd7d7ad45f3c67d81e264373f8c2acc646
Rory Stokes <helix@forscience.com.au>
2013-03-03 16:10:52 +1030

    Reverting broken changes

--- unit.py
+++ unit.py
@@ -4,7 +4,7 @@ import os
 class Unit(pygame.sprite.Sprite):
   def __init__(self,x,y,event,map):
     pygame.sprite.Sprite.__init__(self)
-    self.path     = []
+    self.path     = [(x,y)]
     self.target   = (x,y)
     self.event    = event
     self.map      = map
@@ -17,19 +17,19 @@ class Unit(pygame.sprite.Sprite):
     self.rect.topleft = (x, y)
 
   def move(self,x,y):
-    self.path = self.map.getPath(self.target, (x,y))
+    self.path = self.map.getPath(self.path[0], (x,y))
     
   def update(self):
     reached = True
-
-    if self.rect.left > 64*self.target[0]:
+    print(self.target)
+    if self.rect.left > 64*self.path[0][0]:
       if True: #self.bearing == 90:
         self.rect.move_ip(-self.speed, 0)
       else:
         print("Current bearing "+str(self.bearing))
         self.turnTo(90)
       reached = False
-    elif self.rect.left < 64*self.target[0]:
+    elif self.rect.left < 64*self.path[0][0]:
       if True: #self.bearing == 270:
         self.rect.move_ip(self.speed, 0)
       else:
@@ -37,14 +37,14 @@ class Unit(pygame.sprite.Sprite):
         self.turnTo(270)
       reached = False
     
-    if self.rect.top > 64*self.target[1]:
+    if self.rect.top > 64*self.path[0][1]:
       if True: #self.bearing == 0:
         self.rect.move_ip(0, -self.speed)
       else:
         print("Current bearing "+str(self.bearing))
         self.turnTo(0)
       reached = False
-    elif self.rect.top < 64*self.target[1]:
+    elif self.rect.top < 64*self.path[0][1]:
       if True: #self.bearing == 180:
         self.rect.move_ip(0, self.speed)
       else:
@@ -53,7 +53,7 @@ class Unit(pygame.sprite.Sprite):
       reached = False
       
     if reached:
-      if len(self.path) > 0:
+      if len(self.path) > 1:
         target = self.path.pop(0)
 
   def turnTo(self, targetBearing):
