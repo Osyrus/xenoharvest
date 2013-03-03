cf63b0d1a6807c05c3751789aeb82ac40080213c
Myles Clark <blokart11@gmail.com>
2013-03-03 16:30:29 +1030

    Implemented the rotation of the image.

--- unit.py
+++ unit.py
@@ -2,19 +2,20 @@ import pygame
 import os
 
 class Unit(pygame.sprite.Sprite):
-  def __init__(self,x,y,event,map):
+  def __init__(self,x,y,event,map,image):
     pygame.sprite.Sprite.__init__(self)
     self.path     = []
     self.target   = (x,y)
     self.event    = event
     self.map      = map
-    self.image    = None
+    self.image    = image
+    self.baseImage= self.image
     self.rect     = pygame.Rect(0, 0, 64, 64)
     self.speed    = 4
     self.bearing  = 0
     self.turnSpeed= 10
 
-    self.rect.topleft = (x, y)
+    self.rect.center = (x+32, y+32)
 
   def move(self,x,y):
     self.path = self.map.getPath(self.target, (x,y))
@@ -50,6 +51,8 @@ class Unit(pygame.sprite.Sprite):
     if reached:
       if len(self.path) > 0:
         self.target = self.path.pop(0)
+    
+    self.image = pygame.transform.rotate(self.baseImage, self.bearing)
 
   def turnTo(self, targetBearing):
     if self.bearing < targetBearing:
@@ -70,14 +73,14 @@ class Unit(pygame.sprite.Sprite):
         
 class Player(Unit):
   def __init__(self,x,y,id,event,map):
-    Unit.__init__(self,x,y,event,map)
-
     self.id = id
 
     if id == 0:
-      self.image = pygame.image.load(os.path.join("img","player_b.png"))
+      image = pygame.image.load(os.path.join("img","player_b.png"))
     elif id == 1:
-      self.image = pygame.image.load(os.path.join("img","player_r.png"))
+      image = pygame.image.load(os.path.join("img","player_r.png"))
+
+    Unit.__init__(self,x,y,event,map,image)
 
     event.register("playerMove",self.moveID)
 
